0193c06b7de7baeb8058a36170c2dd2f932e2cf9
ATLAS Tile Demonstrator <atlas.tiledemo@cern.ch>
2017-07-02 23:58:21 +0200

    First commit

--- wh_sensitivity_studies/cuts.sh
+++ wh_sensitivity_studies/cuts.sh
@@ -2,8 +2,11 @@
 
 input_dir=/Users/joakim/ewk/ewk_mbj_ntuples/histfitter/
 version=histfitter_20170521_sig_validation.local
+#input_dir=/Users/joakim/ewk/ewk_mbj_ntuples/histfitter/
+#version=histfitter_20170604_bkg_all.grid.tag2.4.28-0-1-77-gd12fe93/
+
 tag=resolved_MctbbG160_mTbmin_Wh_May30
 
 mkdir -p cuts
 
-rooptimize cut ${input_dir}/${version}/*.merged --supercuts=supercuts/supercuts_${tag}.json -o cuts/cuts_${tag} --numpy --eventWeight weight_mc --weightsFile ../weights_qqbb.json --tree nominal --ncores=8
+echo rooptimize cut ${input_dir}/${version}/*.merged --supercuts=supercuts/supercuts_${tag}.json -o cuts/cuts_${tag} --numpy --eventWeight weight_mc --weightsFile ../weights_qqbb.json --tree nominal --ncores=8
