4b33d29a510b7f6619bd0f6afa395e27ca5b9b98
ATLAS Tile Demonstrator <atlas.tiledemo@cern.ch>
2017-07-03 00:24:54 +0200

    Updated cuts.sh

--- wh_sensitivity_studies/cuts.sh
+++ wh_sensitivity_studies/cuts.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-#input_dir=/Users/joakim/ewk/ewk_mbj_ntuples/histfitter/
-#version=histfitter_20170521_sig_validation.local/
-input_dir=/share/t3data2/jolsson/EWK/ewk_mbj_ntuples/histfitter/
-version=histfitter_20170604_bkg_all.grid.tag2.4.28-0-1-77-gd12fe93/
+input_dir=/Users/joakim/ewk/ewk_mbj_ntuples/histfitter
+#input_dir=/share/t3data2/jolsson/EWK/ewk_mbj_ntuples/histfitter
+version="2.4.28-0-1-77-gd12fe93"
+
+files=$(find ${input_dir}/${version}/ -not -iname *data* | xargs)
 
 tag=resolved_MctbbG160_mTbmin_Wh_May30
 
 mkdir -p cuts
 
-echo rooptimize cut ${input_dir}/${version}/*.merged --supercuts=supercuts/supercuts_${tag}.json -o cuts/cuts_${tag} --numpy --eventWeight weight_mc --weightsFile ../weights_qqbb.json --tree nominal --ncores=8
+rooptimize cut $files --supercuts=supercuts/supercuts_${tag}.json -o cuts/cuts_${tag} --numpy --eventWeight weight_mc --weightsFile ../weights_qqbb.json --tree nominal --ncores=8
+
